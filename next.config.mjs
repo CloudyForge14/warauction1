0a257fb640e5bb7907cbd1c5bcd405e47ac5eb0a
almasezhe <134839836+almasezhe@users.noreply.github.com>
2025-01-18 15:16:42 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
-    return config;
-  },
-};
-
-// Создаём конфигурацию с плагином для интернационализации
+ 
 const withNextIntl = createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+ 
 export default withNextIntl(nextConfig);
