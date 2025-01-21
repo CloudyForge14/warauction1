13d7e7548fe23314b1ca19c6fc4c86bf3f71b6e5
almasezhe <134839836+almasezhe@users.noreply.github.com>
2025-01-21 21:31:58 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+        ],
+      },
+    ];
+  },
+};
+
 export default withNextIntl(nextConfig);
